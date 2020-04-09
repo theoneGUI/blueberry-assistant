331136c630e8f1fe37c901ee6e54b2942a6df58a
lawrencethekitten <49991562+lawrencethekitten@users.noreply.github.com>
2020-04-09 17:22:34 -0500

    Add files via upload

---
+++ inject++.py
@@ -0,0 +1,37 @@
+import socket, threading
+import tkinter as tk
+from tkinter import StringVar
+soc=socket.socket()
+shost=socket.gethostname()
+ip=socket.gethostbyname(shost)
+server_host='totc.ddns.net'
+port=1234
+def listener():
+    quote=StringVar()
+    quote.set("--BEGIN--")
+    sok=socket.socket()
+    server_host='totc.ddns.net'
+    port=1234
+    root = tk.Tk()
+    root.title('incoming')
+    S = tk.Scrollbar(root)
+    T = tk.Text(root, height=4, width=50)
+    S.pack(side=tk.RIGHT, fill=tk.Y)
+    T.pack(side=tk.LEFT, fill=tk.Y)
+    S.config(command=T.yview)
+    T.config(yscrollcommand=S.set)
+    T.insert(tk.END, quote)
+    tk.mainloop()
+    while True:
+        quote=quote+soc.recv(1024).decode()+'\n'
+try:
+    soc.connect((server_host, port))
+    print('Connected as probe\n')
+    soc.send(b"inject")
+except:
+    print('server not available! aborting...')
+#threading.Thread(target=listener).start()
+while True:
+    msg=input('OUTGOING: ')
+    soc.send(msg.encode())
+
